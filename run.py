efc5b99cedcb0c96b4d48dc0a4b8135312b88f78
ivan-pd <93162092+ivan-pd@users.noreply.github.com>
2022-05-09 18:30:54 -0400

    changes made for testing model

--- run.py
+++ run.py
@@ -1,21 +1,22 @@
 import tensorflow as tf
 import numpy as np
 import math
+import os
 
-from hicplus import HiCPlus
-
-import matplotlib.pyplot as plt
-import seaborn as sns
+from model import HiCPlus
+from numpy import savetxt
+# from graphs import plot_pearson
 
 def train(model, train_inputs, train_targets):
 
-    # Shuffling inputs maybe improves accuracy?
+    # Shuffling inputs to prevent overfitting 
     num_examples = train_inputs.shape[0] 
-    # shuffle_indicies = tf.random.shuffle(tf.range(num_examples))
-    # train_inputs = tf.gather(train_inputs, shuffle_indicies)
-    # train_targets = tf.gather(train_targets, shuffle_indicies)
+    shuffle_indicies = tf.random.shuffle(tf.range(num_examples))
+    train_inputs = tf.gather(train_inputs, shuffle_indicies)
+    train_targets = tf.gather(train_targets, shuffle_indicies)
 
     accuracy = []
+    losses =  []
     num_batches = math.floor(num_examples / model.batch_size)
     for i in range(num_batches):
         start = i * model.batch_size
@@ -26,13 +27,14 @@ def train(model, train_inputs, train_targets):
         with tf.GradientTape() as tape: 
             predictions = model.call(input_batch)
             loss = model.loss_function(predictions, target_batch)
+            losses.append(loss)
             accuracy.append(model.accuracy(predictions, target_batch))
 
         train_vars = model.trainable_variables
         gradients = tape.gradient(loss, train_vars)
         model.opt.apply_gradients(zip(gradients, train_vars))
 
-    return accuracy
+    return accuracy, losses
 
 
 def test(model, test_input, test_labels):
@@ -54,91 +56,109 @@ def get_data(file):
         targets = hic_data['target']
         inds = hic_data['inds']
 
-   
     # Reshaping targets to match 28x28 output of HiCPlus Model
-    shape = (inputs.shape[0], 1, 28, 28)
+    shape = (targets.shape[0], 1, 28, 28)
     targets_reshaped = np.zeros(shape)
     for i in range(targets.shape[0]):
         targets_reshaped[i] = targets[i, 0, 6:34, 6:34]
 
-    # Transpose data to convert from (N, C, H, W) to (N, H, W, C) format
-    inputs = tf.transpose(inputs, [0, 2, 3, 1])
-    targets_reshaped = tf.transpose(targets_reshaped, [0, 2, 3, 1])
+    # Squeezing out extra dimentsion  to convert from (N, C, H, W) to (N, H, W, C) format
+    inputs_reshaped = np.squeeze(inputs)
+    targets_reshaped = np.squeeze(targets_reshaped)
 
-    return inputs, targets_reshaped, inds
+    # Adding Channel
+    inputs_reshaped = inputs_reshaped.reshape((inputs_reshaped.shape[0], 40, 40, 1))
+    targets_reshaped = targets_reshaped.reshape((targets_reshaped.shape[0], 28, 28, 1))
+   
+    return inputs_reshaped, targets_reshaped, inds
 
-def get_data2(file):
+def get_baseline_data(file):
     with np.load(file) as hic_data:
         inputs = hic_data['data']
         targets = hic_data['target']
         inds = hic_data['inds']
 
-   
-    # Reshaping targets to match 28x28 output of HiCPlus Model
-    shape = (inputs.shape[0], 1, 28, 28)
+    shape = (targets.shape[0], 1, 28, 28)
+    inputs_reshaped = np.zeros(shape)
     targets_reshaped = np.zeros(shape)
     for i in range(targets.shape[0]):
+        inputs_reshaped[i] = inputs[i, 0, 6:34, 6:34]
         targets_reshaped[i] = targets[i, 0, 6:34, 6:34]
+    
+    inputs_reshaped = np.squeeze(inputs_reshaped)
+    targets_reshaped = np.squeeze(targets_reshaped)
 
-    # Transpose data to convert from (N, C, H, W) to (N, H, W, C) format
-    targets = tf.transpose(targets, [0, 2, 3, 1])
-    targets_reshaped = tf.transpose(targets_reshaped, [0, 2, 3, 1])
+    inputs_reshaped = inputs_reshaped.reshape((inputs_reshaped.shape[0], 28, 28, 1))
+    targets_reshaped = targets_reshaped.reshape((targets_reshaped.shape[0], 28, 28, 1))
+   
+    return inputs_reshaped, targets_reshaped, inds
 
-    return targets, targets_reshaped, inds
 
+# Following save and load model weights functions adapted from VAE Project
 
-def plot_pearson(data):
+def save_model_weights(model, file_name):
+    """
+    Save trained CNN model weights to model_ckpts/
 
-    x = np.arange(len(data))
-    y = data
+    Inputs:
+    - model: Trained CNN model.
+    - args: All arguments.
+    """
 
-    p = sns.lineplot(x, y)
-
-    p.set_xlabel("Batch Number")
-    p.set_ylabel("Pearson Correlation")
-    p.set_title('Average Pearson Correlation Per Batch (Targets vs Targets)')
-    plt.show()
-
+    output_path = os.path.join(file_name)
+    os.makedirs("model_ckpts", exist_ok=True)
+    model.save_weights(output_path)
+   
 
 def main():
-    # inmodel = args.model
-    # hicfile = args.inputfile
-    # outname = args.outputfile
-
-    # inputs, inds = open_data(hicfile)
-    # input1 = inputs[0]
-    # input1 =  np.squeeze(input1)
     
     train_downsample_16 = './synthetic/c40_s28/16_downsampling/train.npz'
     test_downsample_16 = './synthetic/c40_s28/16_downsampling/test.npz'
 
-    # train_inputs, train_targets, train_inds = get_data(train_downsample_16)
-    # test_inputs, test_targets, test_inds = get_data(test_downsample_16)
-
-    train_inputs, train_targets, train_inds = get_data2(train_downsample_16)
-    test_inputs, test_targets, test_inds = get_data2(test_downsample_16)
-    
-    # print(train_inputs.shape)
-    # print(train_targets.shape)
+    train_inputs, train_targets, train_inds = get_data(train_downsample_16)
+    test_inputs, test_targets, test_inds = get_data(test_downsample_16)
 
     model = HiCPlus()
-    epochs = 1
-    train_accuracy = None
+    
+    epochs = 250
+    test_accuracy = []
     for i in range(epochs):
         print(f'Running Epoch: {i}')
-        train_accuracy = train(model, train_inputs, train_targets)
-  
-    # print(f'Max Corr: {np.max(train_accuracy)}')
-    # print(f'Min Corr: {np.min(train_accuracy)}')
+        acc, loss = train(model, train_inputs, train_targets)
 
-    print(train_accuracy)
-    plot_pearson(train_accuracy)
+        # Checking model accuracy every 25 epochs
+        if i % 25 == 0:
+            print(f'Testing Model on Epoch {i}')
+            curr_acc = test(model, test_inputs, test_targets)
+            test_accuracy.append(curr_acc)
 
-    # test_accuracy = test(model, test_inputs, test_targets)
+            file_name = 'acc_' + str(i) + '.csv'
+            savetxt(file_name, test_accuracy, delimiter=',')
 
-    # print(f'Max Test Corr: {np.max(test_accuracy)}')
-    # print(f'Min Test Corr: {np.min(test_accuracy)}')
+        # Saving Final Loss at Epoch 
+        with open('loss.txt', "a+") as file_object:
+            # Move read cursor to the start of file.
+            file_object.seek(0)
 
+            # If file is not empty then append '\n'
+            data = file_object.read(100)
+            if len(data) > 0:
+                file_object.write("\n")
+
+            # Append text at the end of file
+            final_loss = float(loss[-1])
+            file_object.write(str(final_loss))
+            
+            file_object.close()      
+
+        # Saving Model Weights
+        if i % 100 == 0:
+            print(f'Saving Model Weights on Epoch {i}')
+            file_name = 'model_' + str(i)
+            save_model_weights(model, file_name)
+
+            
+    test_accuracy = test(model, test_inputs, test_targets)
 
 
 if __name__ == '__main__':
